94e6826c9c25f8b261c5a1e9d6d6a6c96dbd8c2e
ajPetrov <antoan.baraban@live.com>
2022-12-16 02:52:08 +0200

    Create a transaction.dart class to hold the properties needed for item discription

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,12 +16,30 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> transaction = [
+      Transaction(id: 't1', title: 'Shoes', amount: 69.99, date: DateTime.now(),),
+      Transaction(id: 't2', title: 'Haircut', amount: 69.99, date: DateTime.now(),),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART'),
+              elevation: 5,
+            ),
+          ),
+          Column( children: transaction.map((tx) => Card(
+            child: Text(tx.title),
+          )).toList(),
+          )
+        ],
       ),
     );
   }
