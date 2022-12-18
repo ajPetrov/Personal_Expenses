9b58b7a80c860a57d48dc6e82cb89ae80b892cd9
ajPetrov <antoan.baraban@live.com>
2022-12-18 18:22:22 +0200

    Interpolate the amount string

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MyHomePage extends StatelessWidget {
                                     Border.all(color: Colors.purple, width: 2)),
                             padding: EdgeInsets.all(10),
                             child: Text(
-                              tx.amount.toString(),
+                              '\$${tx.amount}',
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 20,
