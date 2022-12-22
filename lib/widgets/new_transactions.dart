625c948798fe29659e792e242e3dee6cfbbd5b31
ajPetrov <antoan.baraban@live.com>
2022-12-22 22:09:50 +0200

    Seperate the new transactions to a new widget.

---
+++ lib/widgets/new_transactions.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  Function addNewTransaction;
+
+  NewTransaction(this.addNewTransaction);
+
+  @override
+  Widget build(BuildContext context) {
+    final titleController = TextEditingController();
+    final amountController = TextEditingController();
+
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(5),
+        child: Column(children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            controller: titleController,
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Amount'),
+            controller: amountController,
+          ),
+          TextButton(
+            onPressed: addNewTransaction(titleController.text, double.parse(amountController.text)),
+            child: Text(
+                'Add Transaction'
+            ),
+          )
+        ],
+        ),
+      ),
+    );
+  }
+}
