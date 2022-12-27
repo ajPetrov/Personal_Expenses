df08a8be4b5583a041dea916bc408df76161bd8f
ajPetrov <antoan.baraban@live.com>
2022-12-27 23:05:10 +0200

    Change column to listview builder in transaction_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,20 +24,18 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('CHART'),
-                  elevation: 5,
-                ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART'),
+                elevation: 5,
               ),
-              UserTransactions()
-            ],
-          ),
+            ),
+            UserTransactions()
+          ],
         ),
     );
   }
